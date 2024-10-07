1f9d152303a7a2498b1e7eb90ee90ff61b786489
andhika pangestu <78396812+andhika-pangestu@users.noreply.github.com>
2024-10-07 11:29:08 +0700

    All blade template
    Nambahuin semua template

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title', 'Gunung Sembung')</title>
+    @vite('resources/css/app.css')</head>
+<body>
+    <header>
+        @include('partials.header') <!-- Menyertakan header dari partials -->
+    </header>
+
+    <!-- Konten Utama -->
+    <main>
+        @yield('content') <!-- Bagian ini akan diisi konten dari halaman child -->
+    </main>
+
+    <!-- Footer -->
+    <footer>
+        @include('partials.footer') <!-- Menyertakan footer dari partials -->
+    </footer>
+
+    <!-- JavaScript -->
+    <script src="{{ asset('js/app.js') }}"></script>
+</body>
+</html>
