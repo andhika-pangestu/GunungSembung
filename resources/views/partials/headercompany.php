429b3a5b74c6284069ac89513774155737f685c3
Amanda Jayanti Mulyana <ndajayanti1601@gmail.com>
2024-11-30 17:55:09 +0700

    companypage-amanda

---
+++ resources/views/partials/headercompany.php
@@ -0,0 +1,17 @@
+<section class="w-full px-8 text-gray-700 bg-white fixed z-50">
+    <div class="container flex flex-col items-center justify-between py-5 mx-auto md:flex-row max-w-7xl">
+        <div class="relative flex w-full justify-between">
+            <!-- Logo GSP di kiri -->
+            <a href="#_" class="flex items-center mb-5 font-medium text-gray-900 lg:w-auto lg:items-center lg:justify-center md:mb-0">
+                <span class="text-xl font-black leading-none text-red-600 select-none">GUNUNG SEMBUNG PUTRA</span>
+            </a>
+            
+            <!-- Navigasi di kanan -->
+            <nav class="flex flex-wrap items-center text-base ml-auto">
+                    <a href="#sejarah" class="mr-10 font-medium leading-6 text-gray-600 hover:text-gray-900">Sejarah</a>
+                    <a href="#keunggulan" class="mr-10 font-medium leading-6 text-gray-600 hover:text-gray-900">Keunggulan</a>
+                    <a href="#tim" class="mr-10 font-medium leading-6 text-gray-600 hover:text-gray-900">Tim Kami</a>
+            </nav>
+        </div>
+    </div>
+</section>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use App\Http\Controllers\HomeController;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/tour-packages', [HomeController::class, 'allTourPackages'])->name('tour-packages');
+
+Route::get('/company', function () {
+    return view('company');
+})->name('company');
