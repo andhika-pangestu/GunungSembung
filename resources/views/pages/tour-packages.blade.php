f44e4b9cb3c87f7c2395d8ce8957a3e14cfc2637
Mikka <133632967+atnicakim@users.noreply.github.com>
2024-12-16 14:31:02 +0700

    packages done

--- resources/views/pages/tour-packages.blade.php
+++ resources/views/pages/tour-packages.blade.php
@@ -4,6 +4,7 @@
 
 @section('content')
     <main class="w-screen bg-gray-50 flex flex-col items-center justify-start mt-0">
+    <main class="w-screen bg-gray-50 flex flex-col items-center justify-start mt-10">
         <h2 class="text-3xl font-bold text-left m-5 mt-5 text-gray-900 sm:text-4xl w-full pl-20">Semua Paket Wisata</h2>
         <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4 container mt-1">
             @foreach ($slides as $slide)
@@ -20,6 +21,12 @@
                         <a href="{{ $slide['button_link'] }}"
                             class="mt-2 inline-block text-blue-500 font-medium hover:underline">
                             {{ $slide['button_text'] }}
+                        <div class="flex items-center space-x-4">
+                            <span class="text-sm font-bold text-gray-800 mt-4">{{ $slide['price'] }}</span>
+                        </div>
+                        <a href="{{ $slide['button_link'] }}" target="_blank" class="bg-red-500 hover:bg-red-600 text-white text-sm font-semibold px-5 py-1 mt-4 rounded-lg flex items-center justify-center">
+                            {{ $slide['button_text'] }} &nbsp;
+                            <img src = "https://upload.wikimedia.org/wikipedia/commons/5/5e/WhatsApp_icon.png" alt = "WA" class="w-7 h-7 mr-2">
                         </a>
                     </div>
                 </div>
